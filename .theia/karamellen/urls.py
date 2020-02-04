b3abef445e727adcdb9e3f4ed2518eafc1874824
Petra  Mellbrand <petra@mellbrand.com>
2020-02-04 10:50:30 +0000

    add views for products and exhibitions  yin urls.py
    url-patterns for new paths

--- .theia/karamellen/urls.py
+++ .theia/karamellen/urls.py
@@ -19,16 +19,19 @@ from accounts import urls as urls_accounts
 from accounts import urls as urls_products
 from products.views import all_products
 from accounts import urls as urls_exhibitions
-from exhibitions.views import all exhibitions
+from exhibitions.views import all_exhibitions
 from accounts import urls as urls_exhibitioncalendar
 from django.views import static
 from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url('r'^$', all_products, name='index'),
-    url('r'^$', all_exhibitions, name='index'),
+    url('r^$', all_products, name='index'),
     url(r'^accounts/', include(urls_accounts)),
+    url(r'^products/', include(urls_products)),
+    url(r'^exhibitions/', include(urls_exhibitions)),
+    url(r'^media/(?P<path>.*)$', static.serve, {'document_root': MEDIA_ROOT}),
+
 
     # url(r'^exhibitions/', include(urls_exhibitions)),
     # url(r'^exhibitioncalendar/', include(urls_exhibitioncalendar)),
