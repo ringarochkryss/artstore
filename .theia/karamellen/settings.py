8c096f4379afc2de04a51682177e31409ce4d5dd
Petra  Mellbrand <petra@mellbrand.com>
2020-02-11 22:58:32 +0000

    add settings to deploy on aws
    settings for static files

--- .theia/karamellen/settings.py
+++ .theia/karamellen/settings.py
@@ -133,14 +133,15 @@ AWS_S3_OBJECT_PARAMETERS = {
 
 
 AWS_STORAGE_BUCKET_NAME = 'petrasartstore'
-AWS_S3_REGION_NAME = 'EU (Stockholm)'
+AWS_S3_REGION_NAME = 'eu-west-1'
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
@@ -148,8 +149,13 @@ STATICFILES_DIRS = (
 )
 
 
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
 
 STRIPE_PUBLISHABLE = [os.getenv('STRIPE_PUBLISHABLE')]
 STRIPE_SECRET = [os.getenv('STRIPE_SECRET')]
